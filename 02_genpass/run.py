4b07a03420fd1e28419515588fb36041ce6d843f
akrush <akrush24@gmail.com>
2017-07-21 16:45:01 +0300

    Fri Jul 21 16:45:01 MSK 2017

--- 02_genpass/run.py
+++ 02_genpass/run.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
-from random import randint
-from random import getrandbits
-from random import sample
+from random import choice
 
 chnum = "0 1 2 3 4 5 6 7 8 9"
-chstr = "a b c d e f j"
-chc = "?&#@%^"
+chstr = "a b c d e f j h i j k l m n o p q r s t x y z"
+chc = "? & # @ % ^"
 
 pas = ""
 
-for ch in sample((chstr+chstr+chc).split(),4):
-	pas = pas+ch
+pass_ch = (chstr+chstr+chc).split()
+cmin = 4
+
+while True:
+	try:
+		cmax = int(input("Enter password length[min "+str(cmin)+"]: "))
+	except ValueError: pass
+	if cmax >= cmin: break
+
+for ch in range(cmin,cmax):
+	pas = pas+choice(pass_ch)
 print(pas)
