6427f2c8e34c66ebc8985a079924d960ee3aba03
akrush <akrush24@gmail.com>
2017-07-21 17:00:01 +0300

    Fri Jul 21 17:00:01 MSK 2017

--- 02_genpass/run.py
+++ 02_genpass/run.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
+# password generator
 
 from random import choice
 
 chnum = "0 1 2 3 4 5 6 7 8 9"
 chstr = "a b c d e f j h i j k l m n o p q r s t x y z"
-chc = "? & # @ % ^"
+chc = '~ ! @ # $ % ^ & * ( ) _ + ` - = { } [ ] : ; < > . / '
 
 pas = ""
-
-pass_ch = (chstr+chstr+chc).split()
 cmin = 4
+cdef = 8
+cmax = 8
+
+pass_ch = (chnum+chstr+chc+chstr.upper()).split()
 
 while True:
 	try:
-		cmax = int(input("Enter password length[min "+str(cmin)+"]: "))
+		cmax = int(input("Enter password length[min:"+str(cmin)+";def:"+str(cdef)+"]: "))
+		if cmax == "": cmax = 6
 	except ValueError: pass
 	if cmax >= cmin: break
 
-for ch in range(cmin,cmax):
+for ch in range(0,cmax):
 	pas = pas+choice(pass_ch)
 print(pas)
